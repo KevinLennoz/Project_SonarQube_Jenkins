85cc3ef6cf3e27d839d8852acc73716d1a6990f6
Kevin Lennoz <lennoz.kevin@hotmail.fr>
2022-01-04 10:16:35 +0100

    projet initial

---
+++ src/main/java/com/formation/bank/SimpleInterestCalculator.java
@@ -0,0 +1,19 @@
+package com.formation.bank;
+
+public class SimpleInterestCalculator implements InterestCalculator {
+	
+	private double rate;
+
+	public void setRate(double rate) {
+		this.rate = rate;
+
+	}
+
+	public double calculate(double amount, double year) {
+		if (amount < 0 || year < 0) {
+			throw new IllegalArgumentException("Le montant et la durée doivent etre positifs");
+		}
+		return amount * year * rate;
+	}
+
+}
